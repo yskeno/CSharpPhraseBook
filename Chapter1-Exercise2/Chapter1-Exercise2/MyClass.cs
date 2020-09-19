0c18851ee170379879901c67dbc47384e3774998
yskeno <yuu.vi4@gmail.com>
2020-09-19 21:13:02 +0900

    Add Chapter1-Exercise1

---
+++ Chapter1-Exercise2/Chapter1-Exercise2/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter1_Exercise2
+{
+    class MyClass
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    struct MyStruct
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ Chapter1-Exercise2/Chapter1-Exercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chapter1_Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass { X = 1, Y = 2 };
+            var myStruct = new MyStruct { X = 3, Y = 4 };
+
+            PrintObjects(myClass, myStruct);
+        }
+
+        static void PrintObjects(MyClass myClass, MyStruct myStruct)
+        {
+            Console.WriteLine("MyClass X={0} Y={1}, MyStruct X={2} Y={3}", myClass.X, myClass.Y, myStruct.X, myStruct.Y);
+        }
+    }
+}
