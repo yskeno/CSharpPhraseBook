8d402e96c03f2263ae72756a966c8545752de2d3
yskeno <yuu.vi4@gmail.com>
2020-09-19 15:05:59 +0900

    Add Chapter2-Section1 DistanceConverter

---
+++ Chapter2-Section1/DistanceConverter/FeetConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DistanceConverter
+{
+    public static class FeetConverter
+    {
+        private const double ratio = 0.3048;
+
+
+        public static double FromMeter(double meter)
+        {
+            return meter / ratio;
+        }
+
+        public static double ToMeter(double feet)
+        {
+            return feet * ratio;
+        }
+    }
+}
---
+++ Chapter2-Section1/DistanceConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DistanceConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1 && args[0] == "-tom")
+                PrintFeetToMeterList(1, 10);
+            else
+                PrintMeterToFeetList(1, 10);
+        }
+
+
+        static void PrintFeetToMeterList(int start, int stop)
+        {
+            for (int feet = start; feet <= stop; feet++)
+            {
+                double meter = FeetConverter.ToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+
+        static void PrintMeterToFeetList(int start, int stop)
+        {
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = FeetConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+            }
+        }
+    }
+}
