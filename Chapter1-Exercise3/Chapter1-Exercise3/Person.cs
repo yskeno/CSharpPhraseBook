c3ca77cba6a5dafa8a9f231a7c0da71abf885dfb
yskeno <yuu.vi4@gmail.com>
2020-09-22 01:17:38 +0900

    Add Chapter1-Exercise3

---
+++ Chapter1-Exercise3/Chapter1-Exercise3/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Chapter1_Exercise3
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public int GetAge()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - Birthday.Year;
+            if (today < Birthday.AddYears(age))
+                age--;
+            return age;
+        }
+    }
+
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        public string DivisionName { get; set; }
+    }
+
+    public class Student : Person
+    {
+        public int Grade { get; set; }
+        public int ClassNumber { get; set; }
+    }
+}
---
+++ Chapter1-Exercise3/Chapter1-Exercise3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Chapter1_Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee
+            {
+                Id = 100,
+                Name = "Taro Yamada",
+                Birthday = new DateTime(1992, 4, 5),
+                DivisionName = "Sales Division 1",
+            };
+
+            var student = new Student
+            {
+                Name = "John Yamada",
+                Birthday = new DateTime(1985, 6, 4),
+                Grade = 5,
+                ClassNumber = 2,
+            };
+            Console.WriteLine(student.ToString());
+
+            Person personFromStudent = student;
+
+            Object objFromStudent = student;
+        }
+    }
+}
