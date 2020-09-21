27ebd764973d2ec8960625cbcfc485a8a1699068
yskeno <yuu.vi4@gmail.com>
2020-09-21 23:39:30 +0900

    Update Chapter1-Exercise2

--- Chapter1-Exercise2/Chapter1-Exercise2/Program.cs
+++ Chapter1-Exercise2/Chapter1-Exercise2/Program.cs
@@ -10,11 +10,21 @@ namespace Chapter1_Exercise2
             var myStruct = new MyStruct { X = 3, Y = 4 };
 
             PrintObjects(myClass, myStruct);
+
+            // MyStruct is value type variable, so can't change member variable inside function.
+            Console.WriteLine("from Main: MyClass X={0} Y={1}, MyStruct X={2} Y={3}", myClass.X, myClass.Y, myStruct.X, myStruct.Y);
+            PrintObjects(myClass, myStruct);
         }
 
         static void PrintObjects(MyClass myClass, MyStruct myStruct)
         {
-            Console.WriteLine("MyClass X={0} Y={1}, MyStruct X={2} Y={3}", myClass.X, myClass.Y, myStruct.X, myStruct.Y);
+            Console.WriteLine("from Func: MyClass X={0} Y={1}, MyStruct X={2} Y={3}", myClass.X, myClass.Y, myStruct.X, myStruct.Y);
+
+            myClass.X = myClass.X * 2;
+            myClass.Y = myClass.Y * 2;
+            myStruct.X = myStruct.X * 2;
+            myStruct.Y = myStruct.Y * 2;
+
         }
     }
 }
