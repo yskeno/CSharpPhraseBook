9133e09c657e56639c3792f9c44db2a9f666e195
yskeno <yuu.vi4@gmail.com>
2020-09-23 10:10:08 +0900

    Add Chapter2-Exercise1

--- Chapter2-Exercise1/Chapter2-Exercise1/Program.cs
+++ Chapter2-Exercise1/Chapter2-Exercise1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Chapter2_Exercise1
 {
@@ -6,8 +7,19 @@ namespace Chapter2_Exercise1
     {
         static void Main(string[] args)
         {
-            var songs[] = new Song();
-            Console.WriteLine("Hello World!");
+            var songs = new List<Song>();
+            var song1 = new Song("First song", "Yusuke", 358);
+            songs.Add(song1);
+            var song2 = new Song("Second Sons", "yskeno", 866);
+            songs.Add(song2);
+            var song3 = new Song("3rdPlace", "YE", 241);
+            songs.Add(song3);
+
+            foreach (var item in songs)
+            {
+                Console.WriteLine("Title: {0}\nArtist: {1}\nLength: {2}:{3:00}\n", item.Title, item.ArtistName, item.Length / 60, item.Length % 60);
+            }
+            Console.WriteLine("-EOF-");
         }
     }
 }
