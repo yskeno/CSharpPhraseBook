f94562ba161900cf65cedc47c8c86a047e73652f
yskeno <yuu.vi4@gmail.com>
2020-09-23 09:37:36 +0900

    Add Chapter1-Exercise3

---
+++ Chapter2-Exercise1/Chapter2-Exercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chapter2_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var songs[] = new Song();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Chapter2-Exercise1/Chapter2-Exercise1/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter2_Exercise1
+{
+    class Song
+    {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        public Song(string title,string name,int length)
+        {
+            this.Title = title;
+            this.ArtistName = name;
+            this.Length = length;
+        }
+    }
+}
