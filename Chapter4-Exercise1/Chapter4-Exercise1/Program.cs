6dc5161ac0274e568457555724de86803e24074c
yskeno <yuu.vi4@gmail.com>
2020-10-01 01:38:45 +0900

    Add Chapter4 Exercise

---
+++ Chapter4-Exercise1/Chapter4-Exercise1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace Chapter4_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var yearMonths = new YearMonth[]
+            {
+                new YearMonth(1980,1),
+                new YearMonth(1990,4),
+                new YearMonth(2000,7),
+                new YearMonth(2010,9),
+                new YearMonth(2020,12),
+            };
+
+            Console.WriteLine("--Ex4-2-2------");
+            Exercise2_2(yearMonths);
+            Console.WriteLine();
+
+            Console.WriteLine("--Ex4-2-4------");
+            Exercise2_4(yearMonths);
+            Console.WriteLine();
+
+            Console.WriteLine("--Ex4-2-5------");
+            Exercise2_5(yearMonths);
+            Console.WriteLine();
+        }
+
+        private static void Exercise2_2(YearMonth[] yearMonths)
+        {
+            foreach (var item in yearMonths)
+                Console.WriteLine(item);
+        }
+
+        static YearMonth FindFirst21C(YearMonth[] yearMonths)
+        {
+            foreach (var item in yearMonths)
+            {
+                if (item.Is21Century)
+                    return item;
+            }
+            return null;
+        }
+
+        private static void Exercise2_4(YearMonth[] yearMonths)
+        {
+            Console.WriteLine(FindFirst21C(yearMonths)?.ToString() ?? "21世紀のデータはありません");
+        }
+
+        private static void Exercise2_5(YearMonth[] yearMonths)
+        {
+            var newYMs = yearMonths.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach (var item in newYMs)
+                Console.WriteLine($"[{item}]");
+        }
+    }
+}
---
+++ Chapter4-Exercise1/Chapter4-Exercise1/YearMonth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace Chapter4_Exercise1
+{
+    class YearMonth
+    {
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+        public bool Is21Century
+        {
+            get
+            {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        public YearMonth(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+
+        public YearMonth AddOneMonth()
+        {
+            if (Month == 12)
+                return new YearMonth(this.Year + 1, 1);
+            return new YearMonth(this.Year, this.Month + 1);
+        }
+
+        public override string ToString()
+        {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
