b4c044c2b8dbc0a37c7c10035cf4460c8510cc9f
yskeno <yuu.vi4@gmail.com>
2020-10-03 10:07:36 +0900

    Fix Chapter3-Exercise2

--- Chapter3-Exercise2/Chapter3-Exercise2/Program.cs
+++ Chapter3-Exercise2/Chapter3-Exercise2/Program.cs
@@ -8,11 +8,16 @@ namespace Chapter3_Exercise2
     {
         static void Main(string[] args)
         {
-            var names = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", };
+            var names = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
 
             Exercise1(names);
+            Console.WriteLine();
             Exercise2(names);
+            Console.WriteLine();
             Exercise3(names);
+            Console.WriteLine();
             Exercise4(names);
         }
 
@@ -20,16 +25,19 @@ namespace Chapter3_Exercise2
         {
             Console.WriteLine("- Chapter3 Exercise3.2.1 (Index of input line) -");
             Console.Write("Enter a city name: ");
-            var line = Console.ReadLine();
-            Console.WriteLine("{0} is {1} order of names.", line, names.IndexOf(line));
-            Console.WriteLine();
+            do
+            {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+                Console.WriteLine("{0} is {1} order of names.", line, names.IndexOf(line));
+            } while (true);
         }
 
         static void Exercise2(List<string> names)
         {
             Console.WriteLine("- Chapter3 Exercise3.2.2 (Count including 'o') -");
             Console.WriteLine("Number of city including 'o': {0}", names.Count(s => s.Contains("o")));
-            Console.WriteLine();
         }
 
         static void Exercise3(List<string> names)
@@ -38,14 +46,15 @@ namespace Chapter3_Exercise2
             var includeO = names.Where(s => s.Contains("o")).ToArray();
             foreach (var s in includeO)
                 Console.WriteLine(s);
-            Console.WriteLine();
         }
 
         static void Exercise4(List<string> names)
         {
             Console.WriteLine("- Chapter3 Exercise3.2.4 (Number of characters of city name started 'B') -");
-            Console.WriteLine("Number of characters of city name started 'B': {0}", string.Join("", names.Where(s => s.StartsWith("B")).ToArray()).Length);
-            Console.WriteLine();
+            //Console.WriteLine("Number of characters of city name started 'B': {0}", string.Join("", names.Where(s => s.StartsWith("B")).ToArray()).Length);
+            var selected = names.Where(s => s.StartsWith("B")).Select(s => s.Length);
+            foreach (var length in selected)
+                Console.WriteLine("Length of city name started with 'B': {0}", length);
         }
     }
 }
